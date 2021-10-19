a40f33748e3f1ab100fd6e559aafe766f9cb4472
Jakub Zakowicz <j.zakowicz99@gmail.com>
2021-10-19 19:35:23 +0200

    feat(WIT-3): add validation

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,18 +1,22 @@
-const FormInput = ({ icon, label, ...props }) => {
-  return (
-    <div className="flex items-center bg-gray-200 px-4 py-3 rounded-md first:mt-0 mt-5">
-      {icon}
-      <div className="ml-5 w-full">
-        <label htmlFor="login-email" className="text-sm">
-          {label}
-        </label>
-        <input
-          {...props}
-          className="block bg-gray-200 text-md font-bold w-full outline-none"
-        />
-      </div>
+import { Field } from "formik";
+
+const FormInput = ({ icon, label, id, error, ...props }) => (
+  <div className="flex items-center bg-gray-200 px-4 py-3 rounded-md first:mt-0 mt-14 relative">
+    {icon}
+    <div className="ml-5 w-full">
+      <label htmlFor={id} className="text-sm">
+        {label}
+      </label>
+      <Field
+        id={id}
+        {...props}
+        className="block bg-gray-200 text-md font-bold w-full outline-none"
+      />
     </div>
-  );
-};
+    {error ? (
+      <p className="absolute top-20 left-0 text-red-600">{error}</p>
+    ) : null}
+  </div>
+);
 
 export default FormInput;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,6 +4,18 @@ import FacebookIcon from "../images/facebook-icon.svg";
 import { FaEnvelope, FaKey } from "react-icons/fa";
 import FormInput from "../components/FormInput";
 import { Link } from "react-router-dom";
+import { Formik, Form } from "formik";
+import * as Yup from "yup";
+
+const LoginSchema = Yup.object().shape({
+  email: Yup.string().email().required("Email is required!"),
+  password: Yup.string().required("Password is required!"),
+});
+
+const initialValues = {
+  email: "",
+  password: "",
+};
 
 const Login = () => {
   return (
@@ -17,7 +29,10 @@ const Login = () => {
           </span>
         </h1>
 
-        <Link to="/login" className="w-full shadow-loginSocial mt-16 p-5 rounded-lg block text-center">
+        <Link
+          to="/login"
+          className="w-full shadow-loginSocial mt-16 p-5 rounded-lg block text-center"
+        >
           <img
             src={GoogleIcon}
             alt="google_icon"
@@ -27,7 +42,10 @@ const Login = () => {
           Login with Google
         </Link>
 
-        <Link to="/login" className="w-full shadow-loginSocial mt-5 p-5 rounded-lg block text-center">
+        <Link
+          to="/login"
+          className="w-full shadow-loginSocial mt-5 p-5 rounded-lg block text-center"
+        >
           <img
             src={FacebookIcon}
             alt="facebook_icon"
@@ -43,53 +61,69 @@ const Login = () => {
           <span className="w-9/25 h-px bg-gray-300" />
         </div>
 
-        <form className="mt-8">
-          <FormInput
-            icon={<FaEnvelope className="text-3xl" />}
-            id="login-email"
-            label="Email"
-            type="email"
-            placeholder="example@gmail.com"
-          />
-
-          <FormInput
-            icon={<FaKey className="text-3xl" />}
-            id="login-password"
-            label="Password"
-            type="password"
-            placeholder="password"
-          />
-
-          <div className="mt-6 flex justify-between">
-            <div className="flex items center">
-              <input
-                id="login-checkbox"
-                type="checkbox"
-                className="checkbox bg-gray-100 border-gray-300 rounded-md"
+        <Formik
+          initialValues={initialValues}
+          validationSchema={LoginSchema}
+          validateOnChange={false}
+          validateOnBlur={false}
+          onSubmit={values => {
+            console.log(values);
+          }}
+        >
+          {({ errors }) => (
+            <Form className="mt-8">
+              <FormInput
+                icon={<FaEnvelope className="text-3xl" />}
+                id="login-email"
+                name="email"
+                label="Email"
+                type="email"
+                placeholder="example@gmail.com"
+                error={errors.email ? errors.email : null}
               />
-              <label htmlFor="login-checkbox" className="ml-2">
-                Remember me
-              </label>
-            </div>
-            <a href="/" className="text-indigo-600">
-              Forgot Password?
-            </a>
-          </div>
 
-          <button
-            type="submit"
-            className="w-full mt-7 bg-indigo-600 text-white p-5 rounded-lg"
-          >
-            Login
-          </button>
+              <FormInput
+                icon={<FaKey className="text-3xl" />}
+                id="login-password"
+                name="password"
+                label="Password"
+                type="password"
+                placeholder="password"
+                error={errors.password ? errors.password : null}
+              />
 
-          <p className="mt-12 text-center">
-            Don&apos;t have an account?{" "}
-            <Link to="/register" className="text-indigo-600">
-              Register
-            </Link>
-          </p>
-        </form>
+              <div className="mt-16 flex justify-between">
+                <div className="flex items center">
+                  <input
+                    id="login-checkbox"
+                    type="checkbox"
+                    className="checkbox bg-gray-100 border-gray-300 rounded-md"
+                  />
+                  <label htmlFor="login-checkbox" className="ml-2">
+                    Remember me
+                  </label>
+                </div>
+                <Link to="/login" className="text-indigo-600">
+                  Forgot Password?
+                </Link>
+              </div>
+
+              <button
+                type="submit"
+                className="w-full mt-7 bg-indigo-600 text-white p-5 rounded-lg"
+              >
+                Login
+              </button>
+            </Form>
+          )}
+        </Formik>
+
+        <p className="mt-12 text-center">
+          Don&apos;t have an account?{" "}
+          <Link to="/register" className="text-indigo-600">
+            Register
+          </Link>
+        </p>
       </div>
     </div>
   );
