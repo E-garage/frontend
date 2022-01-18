b034e3dc8ce1cb2670a29b026f0fe4339a9c792a
Michal-Kalka <m.kalka26@gmail.com>
2022-01-18 11:11:20 +0100

    Update register_and_login.js

--- cypress/integration/register_and_login.js
+++ cypress/integration/register_and_login.js
@@ -4,16 +4,16 @@ describe('registration', () => {
         cy.visit('localhost:3000/');
         cy.findByRole('link', {  name: /register/i}).click()
         cy.findByRole('textbox', {  name: /name/i}).type('Kacper')
-        cy.findByRole('textbox', {  name: /email/i}).type('kacper123123456789@email.com')
-        cy.get('input[id*="register-password"]').type('kacper123123456789')
-        cy.findByLabelText(/confirm password/i).type('kacper123123456789')
+        cy.findByRole('textbox', {  name: /email/i}).type('kacper123123123@email.com')
+        cy.get('input[id*="register-password"]').type('kacper123123123')
+        cy.findByLabelText(/confirm password/i).type('kacper123123123')
         cy.findByRole('button', {  name: /register/i}).click()
         cy.findByRole('link', {  name: /continue/i}).click()
 
         // logowanie
         
-        cy.findByRole('textbox', {  name: /email/i}).type('kacper123123456789@email.com')
-        cy.findByLabelText(/password/i).type('kacper123123456789')
+        cy.findByRole('textbox', {  name: /email/i}).type('kacper123123123@email.com')
+        cy.findByLabelText(/password/i).type('kacper123123123')
         cy.findByRole('checkbox', {  name: /remember me/i}).check()
         cy.findByRole('button', {  name: /login/i}).click()
 
